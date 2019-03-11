f3f997fb4fbc1e61506f0f1cc5d0669f8019d86b
Tony Jih <tonyjih@gmail.com>
2019-03-11 16:02:50 +0800

    Initial commit

---
+++ package/sdl_mixer/sdl_mixer.mk
@@ -0,0 +1,52 @@
+################################################################################
+#
+# sdl_mixer
+#
+################################################################################
+
+SDL_MIXER_VERSION = 1.2.12
+SDL_MIXER_SOURCE = SDL_mixer-$(SDL_MIXER_VERSION).tar.gz
+SDL_MIXER_SITE =  https://github.com/SDL-mirror/SDL_mixer/archive
+SDL_MIXER_LICENSE = zlib
+SDL_MIXER_LICENSE_FILES = COPYING
+
+SDL_MIXER_INSTALL_STAGING = YES
+SDL_MIXER_DEPENDENCIES = sdl
+SDL_MIXER_CONF_OPTS = \
+	--without-x \
+	--with-sdl-prefix=$(STAGING_DIR)/usr \
+	--disable-music-mp3 \
+	--disable-music-mod \
+	--disable-music-flac # configure script fails when cross compiling
+
+ifeq ($(BR2_PACKAGE_MPG123),y)
+SDL_MIXER_CONF_OPTS += --enable-music-mp3
+SDL_MIXER_DEPENDENCIES += mpg123
+else
+SDL_MIXER_CONF_OPTS += --disable-music-mp3
+endif
+
+# Prefer libmikmod over Modplug due to dependency on C++
+ifeq ($(BR2_PACKAGE_LIBMIKMOD),y)
+SDL_MIXER_CONF_OPTS += --enable-music-mod
+SDL_MIXER_DEPENDENCIES += libmikmod
+else
+ifeq ($(BR2_PACKAGE_LIBMODPLUG),y)
+SDL_MIXER_CONF_OPTS += --enable-music-mod-modplug
+SDL_MIXER_DEPENDENCIES += libmodplug
+else
+SDL_MIXER_CONF_OPTS += --disable-music-mod-modplug
+endif
+endif
+
+# prefer tremor over libvorbis
+ifeq ($(BR2_PACKAGE_TREMOR),y)
+SDL_MIXER_CONF_OPTS += --enable-music-ogg-tremor
+SDL_MIXER_DEPENDENCIES += tremor
+else
+SDL_MIXER_CONF_OPTS += --disable-music-ogg
+endif
+
+SDL_MIXER_CONF_OPTS += --enable-music-timidity-midi
+
+$(eval $(autotools-package))
